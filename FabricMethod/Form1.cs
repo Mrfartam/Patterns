f33f46c05dd8034e7cd57093f57ddfa5f9778ddf
Mrfartam <110575254+Mrfartam@users.noreply.github.com>
2025-02-19 08:55:06 +0700

    Add files via upload

---
+++ FabricMethod/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FabricMethod
+{
+    public partial class Form1 : Form
+    {
+        private string savedText;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            savedText = textBox1.Text;
+            if (!button2.Enabled && !button2.Visible)
+            {
+                button2.Enabled = true;
+                button2.Visible = true;
+                button3.Enabled = true;
+                button3.Visible = true;
+                button4.Enabled = true;
+                button4.Visible = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PushNotificationCreator creator = new PushNotificationCreator();
+            PushNotification notif = creator.GenerateNotification(savedText);
+            notif.SendNotification();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            TelegramNotificationCreator creator = new TelegramNotificationCreator();
+            TelegramNotification notif = creator.GenerateNotification(savedText);
+            notif.SendNotification();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            EmailNotificationCreator creator = new EmailNotificationCreator();
+            EmailNotification notif = creator.GenerateNotification(savedText);
+            notif.SendNotification();
+        }
+    }
+}
---
+++ FabricMethod/Program.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MailKit.Net.Smtp;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Telegram.Bot;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace FabricMethod
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+            System.Windows.Forms.Application.Run(new Form1());
+        }
+    }
+
+    class PushNotificationCreator
+    {
+        public PushNotification GenerateNotification(string txt)
+        {
+            PushNotification notification = new PushNotification(txt);
+            return notification;
+        }
+    }
+
+    class TelegramNotificationCreator
+    {
+        public TelegramNotification GenerateNotification(string txt)
+        {
+            TelegramNotification notification = new TelegramNotification(txt);
+            return notification;
+        }
+    }
+
+    class EmailNotificationCreator
+    {
+        public EmailNotification GenerateNotification(string txt)
+        {
+            EmailNotification notification = new EmailNotification(txt);
+            return notification;
+        }
+    }
+
+    class PushNotification
+    {
+        public string text;
+
+        public PushNotification(string txt)
+        {
+            text = txt;
+        }
+        public void SendNotification()
+        {
+            new ToastContentBuilder().
+                AddText("Новое push-уведомление!").
+                AddText(text).
+                SetToastDuration(ToastDuration.Short).
+                Show();
+        }
+    }
+
+    class TelegramNotification
+    {
+        public string text;
+        public string nickname;
+        public TelegramBotClient bot;
+
+        public TelegramNotification(string txt)
+        {
+            text = txt;
+            nickname = "1280753537";
+            bot = new TelegramBotClient("7746664717:AAEOqZgxkvMPZv_7Xl5Btgkhv5chebRsr2g");
+        }
+        public async void SendNotification()
+        {
+            try
+            {
+                await bot.SendMessage(nickname, $"Новое уведомление в телеграме!\n{text}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+
+    class EmailNotification
+    {
+        public string text;
+        public string sender_email;
+        public string receiver_email;
+
+        public EmailNotification(string txt)
+        {
+            text = txt;
+            sender_email = "notification.ooad@gmail.com";
+            receiver_email = "parfenovivan55@gmail.com";
+        }
+        public async void SendNotification()
+        {
+            string smtpServer = "smtp.gmail.com";
+            int smtpPort = 465;
+            bool useSsl = true;
+
+            string sender_pass = "xkae rird aewv nzzu";
+
+            string subject = "Новое уведомление на почту!";
+            string body = text;
+
+            try
+            {
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress("Notification", sender_email));
+                message.To.Add(new MailboxAddress("Получатель", receiver_email));
+                message.Subject = subject;
+
+                message.Body = new TextPart("plain")
+                {
+                    Text = body
+                };
+                
+                using (var client = new SmtpClient())
+                {
+                    await client.ConnectAsync(smtpServer, smtpPort, useSsl);
+                    await client.AuthenticateAsync(sender_email, sender_pass);
+                    await client.SendAsync(message);
+                    await client.DisconnectAsync(true);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
